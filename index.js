42693222197ed8824415c18212ff946f210c116c
Jean Paul <jeanpaulrodriguezb@gmail.com>
2022-04-06 11:24:14 -0400

    Contador de tren subterraneo

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let countEl = document.getElementById("count-el")
+
+let saveEl = document.getElementById("save-el")
+
+let count = 0 
+function increment() 
+{
+
+count += 1
+countEl.innerText = count 
+
+}
+ function save()
+ {
+     let saveDash = count + " - "
+     saveEl.textContent += saveDash
+     count = 0
+     countEl.innerText = count
+ }
